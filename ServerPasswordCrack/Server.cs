6586c7907548b2dbe5cdd785eda55df3daa26d94
Resul134 <resulkurrr134@gmail.com>
2020-03-06 13:50:17 +0100

    update

--- ServerPasswordCrack/Server.cs
+++ ServerPasswordCrack/Server.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Xml;
 using Client.model;
@@ -13,12 +15,12 @@ namespace ServerPasswordCrack
     public class Server
     {
 
-        private static int count = 0;
+        private static int count = 0; 
         private List<UserInfo> userInfos;
 
         public void Start(int port)
         {
-            TcpListener socket = new TcpListener(IPAddress.Loopback, port);
+            TcpListener socket = new TcpListener(IPAddress.Parse("192.168.24.136"), port);
             userInfos = ReadPasswordFile("passwords.txt");
             
 
@@ -84,31 +86,39 @@ namespace ServerPasswordCrack
 
             using (StreamReader reader = new StreamReader(tempsocket.GetStream()))
             using (StreamWriter writer = new StreamWriter(tempsocket.GetStream()))
-            {            
+            {
+
+                Stopwatch watch = Stopwatch.StartNew();
+
+                TimeSpan passedTime = watch.Elapsed;
 
 
-                
-                
-
-                
                 while (true)
                 {
                     string hello = reader.ReadLine();
                     Console.WriteLine(hello);
                     if (hello != null && hello == "Hej")
                     {
+                        
 
+                        
 
                         writer.WriteLine(userInfos[count].ToString());
                         writer.Flush();
+
+                        count++;
 
                         string dznuts = await reader.ReadLineAsync();
 
                         Console.WriteLine(dznuts);
 
+
+                        Thread.Sleep(100);
+                        Console.WriteLine(passedTime);
+
                     }
 
-                    count++;
+                    
 
 
                 }
